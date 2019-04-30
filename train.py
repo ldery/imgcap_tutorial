e6646a12eb8e9630c702da849293b5de0f6f65e8
Lucio Dery <ldery@stanford.edu>
2019-04-30 07:24:06 +0000

    .

--- train.py
+++ train.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     parser.add_argument('--image_dir', type=str, default='data/resized2014', help='directory for resized images')
     parser.add_argument('--caption_path', type=str, default='data/annotations/captions_train2014.pkl', help='path for train annotation json file')
     parser.add_argument('--log_step', type=int , default=10, help='step size for prining log info')
-    parser.add_argument('--save_step', type=int , default=1000, help='step size for saving trained models')
+    parser.add_argument('--save_step', type=int , default=100, help='step size for saving trained models')
 
     # Model parameters
     parser.add_argument('--embed_size', type=int , default=256, help='dimension of word embedding vectors')
