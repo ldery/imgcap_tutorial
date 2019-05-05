fbdb2564d2910a36395ddd8cdf03c1f468c5930b
Lucio Dery <derylucio@devfair0248.h2.fair>
2019-05-05 15:45:17 -0700

    final clean version

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 wget https://www.dropbox.com/s/lmwbhnjrxbvoo01/Data.zip
-unzip Data.zip
+unzip -q Data.zip
 rm Data.zip
 
-mkdir models
-wget https://www.dropbox.com/s/ne0ixz5d58ccbbz/pretrained_model.zip -P ./models/
-unzip ./models/pretrained_model.zip -d ./models/
-rm ./models/pretrained_model.zip
+wget https://www.dropbox.com/s/k140fpuysceh1li/pretrainedmodels.zip
+unzip -q pretrainedmodels.zip
+mv pretrainedmodels models
+rm pretrainedmodels.zip
